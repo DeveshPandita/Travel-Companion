29b5723ca40366207531d00bab48e4f12716fac5
DeveshPanditaC <deveshpp21@iitk.ac.inC>
2024-10-05 16:28:01 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+//Function that will make the api call:
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': 'a47ffcae23msh1ff5af116b99498p1e1341jsn9fa3d0b47c82',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
